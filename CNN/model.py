c23570bdb2142b27e1920469f2e3adb440b4edaa
unknown <mikecwen@gmail.com>
2026-02-22 18:30:25 -0800

    adding the model itself

--- CNN/model.py
+++ CNN/model.py
@@ -23,4 +23,17 @@ print(len(model.layers))  # 2
 https://www.geeksforgeeks.org/deep-learning/difference-between-ann-cnn-and-rnn/
 explaines the covolutions
 https://docs.pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
 """
